eaa8db8530f290108186e531dafbb930d7c35eeb
sergeponomaryov <ponomaryovserge@gmail.com>
2020-05-11 18:01:54 +0200

    manual markup

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Composer = require('telegraf/composer')
 const session = require('telegraf/session')
 const data = require('wine-pairing/src/data.json')
+const Telegraf = require('telegraf')
+const { Markup } = Telegraf
 
 const bot = new Composer()
 
@@ -9,37 +11,26 @@ const Keyboard = require('telegraf-keyboard');
 
 bot.use(session())
 
-const main = data.ingredients.filter(ingredient => ingredient.type == "main")
-const secondary = data.ingredients.filter(ingredient => ingredient.type == "secondary")
-const prep = data.ingredients.filter(ingredient => ingredient.type == "prep")
-const spice = data.ingredients.filter(ingredient => ingredient.type == "spice")
+// const main = data.ingredients.filter(ingredient => ingredient.type == "main")
+// const secondary = data.ingredients.filter(ingredient => ingredient.type == "secondary")
+// const prep = data.ingredients.filter(ingredient => ingredient.type == "prep")
+// const spice = data.ingredients.filter(ingredient => ingredient.type == "spice")
 
-let keyboards = {1: createKeyboard(main), 2: createKeyboard(secondary), 3: createKeyboard(prep), 4: createKeyboard(spice)};
-
-function createKeyboard(ingredients) {
-    let keyboard = new Keyboard({inline: true});
-    for (i = 0; i < ingredients.length; i++) {
-        // rewrite this india a bit
-        let add = [`${ingredients[i].label}:${ingredients[i].id}`];
-        if(ingredients[i+1] !== undefined) add.push(`${ingredients[i+1].label}:${ingredients[i+1].id}`);
-
-        keyboard.add(add)
-        i++;
-    }
-    return keyboard;
+let buttons = [];
+for (i = 0; i < main.length; i++) {
+    buttons.push([Markup.callbackButton(main[i].label, (main[i].id !== null) ? main[i].id : "skip")])
 }
+
+const inlineMessageRatingKeyboard = Markup.inlineKeyboard(buttons).oneTime().resize().extra()
 
 let selection = [];
 let step = 1;
 
 bot.start((ctx) => {
-    ctx.reply('Simple Keyboard', keyboards[step].draw())
-})
-.on('callback_query', (ctx) => {
-    step++;
-    selection.push(ctx.callbackQuery.data)
-    // change existing keyboard instead!
-    ctx.reply('Simple Keyboard', keyboards[step].draw())
+    ctx.telegram.sendMessage(
+        ctx.from.id,
+        'Like?',
+        inlineMessageRatingKeyboard)
 })
 
 module.exports = bot
