6cf22a2c375fd62c97a1b0b5b33093a9bd9a774c
sergeponomaryov <ponomaryovserge@gmail.com>
2020-05-11 18:22:58 +0200

    Revert "Update index.js"
    This reverts commit 2dc619018977220f2fdeb22f1b95aee49e3768ed.

--- index.js
+++ index.js
@@ -11,27 +11,26 @@ const Keyboard = require('telegraf-keyboard');
 
 bot.use(session())
 
-const ingredients = data.ingredients.filter(ingredient => ingredient.id != null).map(a => a.label)
+// const main = data.ingredients.filter(ingredient => ingredient.type == "main")
+// const secondary = data.ingredients.filter(ingredient => ingredient.type == "secondary")
+// const prep = data.ingredients.filter(ingredient => ingredient.type == "prep")
+// const spice = data.ingredients.filter(ingredient => ingredient.type == "spice")
 
-const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`)
-menu.setCommand('start')
+let buttons = [];
+for (i = 0; i < main.length; i++) {
+    buttons.push([Markup.callbackButton(main[i].label, (main[i].id !== null) ? main[i].id : "skip")])
+}
 
-menu.select('I am excited!', ingredients, {
-    setPage: (ctx, page) => {
-        ctx.session.page = page
-    },
-    getCurrentPage: ctx => ctx.session.page,
-    columns: 2,
-    maxRows: 5
+const inlineMessageRatingKeyboard = Markup.inlineKeyboard(buttons).oneTime().resize().extra()
+
+let selection = [];
+let step = 1;
+
+bot.start((ctx) => {
+    ctx.telegram.sendMessage(
+        ctx.from.id,
+        'Like?',
+        inlineMessageRatingKeyboard)
 })
-menu.pagination('page', {
-    setPage: (ctx, page) => {
-      ctx.session.page = page
-    },
-    getTotalPages: ctx => ctx.session.totalPages,
-    getCurrentPage: ctx => ctx.session.page
-})
-
-bot.use(menu.init())
 
 module.exports = bot
