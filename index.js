8f56f2cc6375390b860b272e25b4633a797b57b7
sergeponomaryov <ponomaryovserge@gmail.com>
2020-05-13 23:04:23 +0200

    mongoose, run without composer, fix newlines in results

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Composer = require('telegraf/composer')
 const session = require('telegraf/session')
 const data = require('wine-pairing/src/data.json')
@@ -5,8 +6,14 @@ const logic = require('wine-pairing/src/logic.js')
 const Telegraf = require('telegraf')
 const { Markup } = Telegraf
 
-const bot = new Composer()
+const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.use(session())
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const Schema = mongoose.Schema;
+const userSchema = new Schema({chatId: Number, name: String, username: String, language: String, lastUsed: Number});
+const User = mongoose.model("User", userSchema);
 
 const main = data.ingredients.filter(ingredient => ingredient.type == "main")
 const secondary = data.ingredients.filter(ingredient => ingredient.type == "secondary")
@@ -28,27 +35,38 @@ const stepTexts = {
     4: 'Any spices? 🌶'
 }
 
+function logUser(ctx) {
+    User.findOneAndUpdate({chatId: ctx.from.id}, {
+        name: ctx.from.first_name + " " + ctx.from.last_name,
+        username: ctx.from.username,
+        language: ctx.from.language_code,
+        lastUsed: Math.floor(Date.now() / 1000)
+    }, {new: true, upsert: true}, function(err, data) {
+        if (err) return console.error(err);
+        else return true;
+    });
+}
+
 bot.start((ctx) => {
+    logUser(ctx);
     ctx.session.step = 1;
     ctx.session.selections = [];
     return ctx.reply(stepTexts[ctx.session.step], keyboards[ctx.session.step])
 })
 .command('start', (ctx) => {
+    logUser(ctx);
     ctx.session.step = 1;
+    ctx.session.selections = [];
     return ctx.reply(stepTexts[ctx.session.step], keyboards[ctx.session.step])
 })
 .command('help', (ctx) => {
-    return ctx.replyWithMarkdown("Wine goes with food. The right pairing will bring out the best in both of them. The wrong one, and you won't taste either. It's not magic, it's chemistry. It has to do with acids and fats, sugars and spices, alcohols and tannins. But you don't have to know all that. I will help you. 😌\nThe way it works is you pick what you're eating, i.e. main dish, side, how it's prepared and spices, and it picks the best wine type match (e.g. Bold Reds, with examples of grapes) based on all of these, with the main ingredient having the most weight of course. It also shows which wine types matched with which ingredients. 🍷\n\nJust try it: /start\nI'm also available as a [website](https://wine.lisik.dev) and as an [Android app](https://play.google.com/store/apps/details?id=com.lisik.winepairing).\nTo contact the developer: @sergeponomaryov", {"disable_web_page_preview": true})
+    return ctx.replyWithMarkdown("Wine goes with food. The right pairing will bring out the best in both of them. The wrong one, and you won't taste either. It's not magic, it's chemistry. It has to do with acids and fats, sugars and spices, alcohols and tannins. But you don't have to know all that. I will help you. 😌\nThe way it works is you pick what you're eating, i.e. main dish, side, how it's prepared and spices, and it picks the best wine type match (e.g. Bold Reds, with examples of grapes) based on all of these, with the main ingredient having the most weight of course. It also shows which wine types matched with which ingredients. 🍷\n\nJust try it: /start\n\nI'm also available as a [website](https://wine.lisik.dev) and as an [Android app](https://play.google.com/store/apps/details?id=com.lisik.winepairing).\nIcon made by Freepik from www.flaticon.com\nTo contact the developer: @sergeponomaryov", {"disable_web_page_preview": true})
 })
 .command('back', (ctx) => {
-    ctx.session.step--;
-    ctx.session.selections[ctx.session.step - 1] = null; // zero based
-    return ctx.reply(stepTexts[ctx.session.step], keyboards[ctx.session.step])
+    return back(ctx);
 })
 .hears('⏪ Back', (ctx) => {
-    ctx.session.step--;
-    ctx.session.selections[ctx.session.step - 1] = null; // zero based
-    return ctx.reply(stepTexts[ctx.session.step], keyboards[ctx.session.step])
+    return back(ctx);
 })
 .on('text', (ctx) => {
     let obj = data.ingredients.find(x => x.label == ctx.message.text)
@@ -64,13 +82,13 @@ bot.start((ctx) => {
             // show results
             ctx.session.selections = ctx.session.selections.filter(x => x); // remove null values
             let results = logic.rankWineTypes(ctx.session.selections);
-            let resp = 'Try these wines 🍷\n\n';
+            let resp = 'Try these wines 🍷';
             results.forEach(result => {
-                resp += `*${result.name}* _like ${result.examples.join(', ')}_\n`;
-                resp += `*${result.match}%* match\n`;
-                if(result.perfectMatches.length) resp += `_Perfect with ${result.perfectMatches.join(' and ')}_\n\n`;
+                resp += `\n\n*${result.name}* _like ${result.examples.join(', ')}_\n`;
+                resp += `*${result.match}%* match`;
+                if(result.perfectMatches.length) resp += `\n_Perfect with ${result.perfectMatches.join(' and ')}_`;
             });
-            resp += '\nWas this useful? Share me with your friends, or /start over 👍'
+            resp += '\n\nWas this useful? Share me with your friends, or /start over 👍'
             return ctx.replyWithMarkdown(resp, Markup.removeKeyboard().extra())
         }
     }
@@ -79,4 +97,10 @@ bot.start((ctx) => {
     }
 })
 
-module.exports = bot
+function back(ctx) {
+    ctx.session.step--;
+    ctx.session.selections[ctx.session.step - 1] = null; // zero based
+    return ctx.reply(stepTexts[ctx.session.step], keyboards[ctx.session.step])
+}
+
+bot.launch()
